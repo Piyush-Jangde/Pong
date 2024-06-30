e01bf34c763804e20135eb93a588932b2b5b22a0
Piyush Jangde <70879352+Piyush-Jangde@users.noreply.github.com>
2024-06-30 16:18:20 +0530

    Uploaded Pong

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+
+    private int _playerScore;
+    private int _computerScore;
+
+    public Text playerScoreText;
+    public Text computerScoreText;
+
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+
+    public void PlayerScores()
+    {
+        _playerScore++;
+        this.playerScoreText.text = _playerScore.ToString();
+        ResetRound();
+    }
+    public void ComputerScores() 
+    {
+        _computerScore++;
+        this.computerScoreText.text = _computerScore.ToString();
+        ResetRound();
+    }
+    private void ResetRound()
+    {
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+}
